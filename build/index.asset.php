5c5aa6ca08f32431da33a483759210da2b5f7ae2
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-10-07 23:27:47 +0200

    forgot to build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '1cea05529e02f34a6e69');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'b47bce01b6522c54382e');
